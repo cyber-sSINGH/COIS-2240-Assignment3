8479c58f074af593b3ad18709ed057604e5519da
cyber-sSINGH <71836518+cyber-sSINGH@users.noreply.github.com>
2025-11-30 23:13:21 -0500

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,8 +1,12 @@
+// Car.java
 public class Car extends Vehicle implements Rentable {
     private int numSeats;
 
     public Car(String make, String model, int year, int numSeats) {
         super(make, model, year);
+        if (numSeats <= 0) {
+            throw new IllegalArgumentException("numSeats must be greater than zero.");
+        }
         this.numSeats = numSeats;
     }
 
@@ -10,20 +14,21 @@ public class Car extends Vehicle implements Rentable {
         return numSeats;
     }
 
+    // Override getInfo to include number of seats at the end
     @Override
     public String getInfo() {
-        return super.getInfo() + " | Seats: " + numSeats;
+        return super.getInfo() + "\t" + numSeats;
     }
 
     @Override
     public void rentVehicle() {
-        setStatus(VehicleStatus.Rented);
+        setStatus(Status.Rented);
         System.out.println("Car " + getLicensePlate() + " has been rented.");
     }
 
     @Override
     public void returnVehicle() {
-        setStatus(VehicleStatus.Available);
+        setStatus(Status.Available);
         System.out.println("Car " + getLicensePlate() + " has been returned.");
     }
 }
