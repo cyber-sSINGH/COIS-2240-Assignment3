94bf463e0c3c3103a0b70ea001135b75240ca59f
cyber-sSINGH <71836518+cyber-sSINGH@users.noreply.github.com>
2025-11-30 23:30:32 -0500

    Update PickupTruck.java

--- PickupTruck.java
+++ PickupTruck.java
@@ -1,36 +1,36 @@
+// PickupTruck.java
 public class PickupTruck extends Vehicle implements Rentable {
-    private double cargoSize;
-    private boolean hasTrailer;
+
+    private double cargoSize;      
+    private boolean hasTrailer;    
 
     public PickupTruck(String make, String model, int year, double cargoSize, boolean hasTrailer) {
         super(make, model, year);
-        if (cargoSize <= 0) throw new IllegalArgumentException("Cargo size must be > 0");
+        if (cargoSize <= 0) {
+            throw new IllegalArgumentException("cargoSize must be greater than zero.");
+        }
         this.cargoSize = cargoSize;
         this.hasTrailer = hasTrailer;
     }
 
-    public double getCargoSize() {
-        return cargoSize;
-    }
-
-    public boolean hasTrailer() {
-        return hasTrailer;
-    }
+    public double getCargoSize() { return cargoSize; }
+    public boolean hasTrailer() { return hasTrailer; }
 
     @Override
     public String getInfo() {
-        return super.getInfo() + " | Cargo Size: " + cargoSize + " | Has Trailer: " + (hasTrailer ? "Yes" : "No");
+        return super.getInfo() + "\t" + cargoSize + "ft";
     }
-
+    
+    // Implementation of Rentable interface methods
     @Override
     public void rentVehicle() {
-        setStatus(VehicleStatus.Rented);
-        System.out.println("Pickup Truck " + getLicensePlate() + " has been rented.");
+        setStatus(Status.Rented);
+        System.out.println("PickupTruck " + getLicensePlate() + " has been rented.");
     }
 
     @Override
     public void returnVehicle() {
-        setStatus(VehicleStatus.Available);
-        System.out.println("Pickup Truck " + getLicensePlate() + " has been returned.");
+        setStatus(Status.Available);
+        System.out.println("PickupTruck " + getLicensePlate() + " has been returned.");
     }
 }
